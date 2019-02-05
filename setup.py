eaa58d1cda0175ba17f662c0b470ad287b4bbdd2
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2019-02-05 11:45:18 -0600

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ install = [
 
 setup(
     name='recipe',
-    version='0.3.0',
+    version='0.3.1',
     description='Lego construction kit for SQL',
     long_description=(open('README.rst').read()),
     author='Chris Gemignani',
