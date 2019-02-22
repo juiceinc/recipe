21994c715039595a8c9faf7f1bc2ae9103912215
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2019-02-22 07:51:21 -0600

    Bump version: 0.4.0 → 0.5.0

--- recipe/__init__.py
+++ recipe/__init__.py
@@ -36,7 +36,7 @@ except ImportError:
 
 logging.getLogger(__name__).addHandler(NullHandler())
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 __all__ = [
     'BadIngredient',
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ install = [
 
 setup(
     name='recipe',
-    version='0.4.0',
+    version='0.5.0',
     description='Lego construction kit for SQL',
     long_description=(open('README.rst').read()),
     author='Chris Gemignani',
