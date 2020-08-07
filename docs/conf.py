aa0247ad74cfc76bb5b37c56ef61f083979bb582
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2020-08-07 12:59:30 -0500

    Bump version: 0.18.0 → 0.18.1

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ project = "Recipe"
 year = "2019"
 author = "Chris Gemignani"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.18.0"
+version = release = "0.18.1"
 
 pygments_style = "trac"
 templates_path = ["."]
