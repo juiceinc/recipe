8a98550dddc66bec79683c5f53918ea0a8066e1e
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2020-10-01 08:24:12 -0500

    changes

--- recipe/core.py
+++ recipe/core.py
@@ -135,8 +135,8 @@ class Recipe(object):
         count_query = self._session.query(func.count().label("count")).select_from(
             query.subquery()
         )
-        count_query._cache_region = self._cache_region
-
+        if hasattr(query, "_cache_region"):
+            count_query._cache_region = query._cache_region
         if hasattr(query, "region"):
             count_query.region = query.region
         if hasattr(query, "cache_prefix"):
