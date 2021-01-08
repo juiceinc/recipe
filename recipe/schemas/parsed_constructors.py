b5aea8fca2c793aed68c4dedf1216415652722dc
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2021-01-08 10:45:08 -0600

    More cleanup

--- recipe/schemas/parsed_constructors.py
+++ recipe/schemas/parsed_constructors.py
@@ -1,224 +1,12 @@
 """Convert parsed trees into SQLAlchemy objects """
-from lark import Transformer, v_args
-from lark.exceptions import GrammarError, LarkError
-from sqlalchemy import func, case, and_, or_, not_, cast, Float
 from datetime import date
 
-from .utils import (
-    generate_lookup_by_engine,
-    find_column,
-    ingredient_class_for_name,
-    convert_value,
-    calc_date_range,
-)
-from .engine_support import aggregations_by_engine, conversions_by_engine
+from lark.exceptions import GrammarError, LarkError
+
 from recipe.exceptions import BadIngredient
 from recipe.ingredients import InvalidIngredient
 
-
-@v_args(inline=True)  # Affects the signatures of the methods
-class TransformToSQLAlchemyExpression(Transformer):
-    """Converts a field to a SQLAlchemy expression """
-
-    # We have rules named "add", "sub", "mul", and "neg" in our grammar; Transformer
-    # dispatches to these.
-    from operator import add, sub, mul, neg
-
-    def __init__(self, selectable, require_aggregation=False):
-        self.selectable = selectable
-        self.require_aggregation = require_aggregation
-        # Database driver
-        try:
-            self.drivername = selectable.metadata.bind.url.drivername
-        except Exception:
-            self.drivername = "unknown"
-
-        self.aggregations = generate_lookup_by_engine(
-            aggregations_by_engine, self.drivername
-        )
-        self.conversions = generate_lookup_by_engine(
-            conversions_by_engine, self.drivername
-        )
-
-    def number(self, value):
-        try:
-            return int(value)
-        except ValueError:
-            return float(value)
-
-    def true(self, value):
-        return True
-
-    def false(self, value):
-        return False
-
-    def IN(self, value):
-        return "IN"
-
-    def NOTIN(self, value):
-        return "NOTIN"
-
-    def IS(self, value):
-        return "IS"
-
-    def NULL(self, value):
-        return None
-
-    def BETWEEN(self, value):
-        return "BETWEEN"
-
-    def aggregate(self, name):
-        """Return a callable that generates SQLAlchemy to aggregate a field.
-
-        Aggregations may be database specific
-        """
-        ag = self.aggregations.get(name.lower())
-        if ag is None:
-            raise ValueError(
-                "Aggregation {} is not supported on {} engine".format(
-                    name, self.drivername
-                )
-            )
-        return ag
-
-    def div(self, num, denom):
-        """SQL safe division"""
-        if isinstance(denom, (int, float)):
-            if denom == 0:
-                raise ValueError("Denominator can not be zero")
-            elif isinstance(num, (int, float)):
-                return num / denom
-            else:
-                return cast(num, Float) / denom
-        else:
-            if isinstance(num, (int, float)):
-                return case([(denom == 0, None)], else_=num / cast(denom, Float))
-            else:
-                return case(
-                    [(denom == 0, None)], else_=cast(num, Float) / cast(denom, Float)
-                )
-
-    def column(self, name):
-        return find_column(self.selectable, name)
-
-    def agex(self, aggr, val):
-        if val == "*":
-            return func.count()
-        else:
-            return aggr(val)
-
-    def conversion(self, name):
-        conv_fn = self.conversions.get(name.lower())
-        if conv_fn is None:
-            raise ValueError(
-                "Conversion {} is not supported on {} engine".format(
-                    conv_fn, self.drivername
-                )
-            )
-        return conv_fn
-
-    def convertedcol(self, conversion, col):
-        conv_fn = self.conversions.get(conversion.lower())
-        if conv_fn is None:
-            raise ValueError(
-                "Conversion {} is not supported on {} engine".format(
-                    conv_fn, self.drivername
-                )
-            )
-        return conv_fn(col)
-
-    def expr(self, expr):
-        return expr
-
-    def case(self, *args):
-        args = list(args)
-
-        # If there's an odd number of args, pop the last one to use as the else
-        if len(args) % 2:
-            else_expr = args.pop()
-        else:
-            else_expr = None
-
-        # collect the other args into pairs
-        # ['a','b','c','d'] --> [('a',b'), ('c','d')]
-        pairs = zip(args[::2], args[1::2])
-        return case(pairs, else_=else_expr)
-
-    def is_comparison(self, *args):
-        """Things that can be compared with IS
-
-        will be either IS NULL or an intelligent date
-        like "IS prior year" or "IS next week".
-
-        If intelligent dates are used, determine the relevant dates and
-        return a tuple of start_date, end_date
-        """
-        if len(args) == 1 and args[0] is None:
-            return None
-        else:
-            # INTELLIGENT_DATE_OFFSET: /prior/i | /current/i | /next/i
-            # INTELLIGENT_DATE_UNITS: /ytd/i | /year/i | /qtr/i | /month/i | /week/i | /day/i
-            offset, units = str(args[0]).lower(), str(args[1]).lower()
-            return calc_date_range(offset, units, date.today())
-
-    def relation_expr(self, left, rel, right):
-        rel = rel.lower()
-        comparators = {
-            "=": "__eq__",
-            ">": "__gt__",
-            ">=": "__ge__",
-            "!=": "__ne__",
-            "<": "__lt__",
-            "<=": "__le__",
-        }
-        if right is None:
-            return
-        # Convert the right into a type compatible with the left
-        right = convert_value(left, right)
-        return getattr(left, comparators[rel])(right)
-
-    def relation_expr_using_is(self, left, rel, right):
-        """A relation expression like age is null or
-        birth_date is last month"""
-        # TODO: Why is this not handled by the tokenization
-        if str(right).upper() == "NULL" or right is None:
-            return left.is_(None)
-        else:
-            return left.between(*right)
-
-    def array(self, *args):
-        # TODO  check these are all the same type
-        # And match the type of the column!
-        return args
-
-    def vector_relation_expr(self, left, rel, right):
-        comparators = {"IN": "in_", "NOTIN": "notin_"}
-        return getattr(left, comparators[rel.upper()])(right)
-
-    def between_relation_expr(self, col, between, low, _, high):
-        # TODO: check data types and convert dates.
-        return col.between(convert_value(col, low), convert_value(col, high))
-
-    def bool_expr(self, *exprs):
-        if len(exprs) > 1:
-            left, _, right = exprs
-            return or_(left, right)
-        else:
-            return exprs[0]
-
-    def string_literal(self, value):
-        # Strip the quotes off of this string
-        return value.value[1:-1]
-
-    def bool_term(self, *exprs):
-        if len(exprs) > 1:
-            left, _, right = exprs
-            return and_(left, right)
-        else:
-            return exprs[0]
-
-    def not_bool_factor(self, notval, expr):
-        return not_(expr)
+from .utils import ingredient_class_for_name
 
 
 def _convert_bucket_to_field(field, bucket, buckets_default_label, builder):
@@ -270,10 +58,6 @@ def _convert_bucket_to_field(field, bucket, buckets_default_label, builder):
     return "if(" + ",".join(parts) + ")", "if(" + ",".join(order_by_parts) + ")"
 
 
-def parse_or_bad(builder, text, **kwargs):
-    pass
-
-
 def create_ingredient_from_parsed(ingr_dict, builder, debug=False):
     """ Create an ingredient from config version 2 object . """
     kind = ingr_dict.pop("kind", "metric")
@@ -282,9 +66,6 @@ def create_ingredient_from_parsed(ingr_dict, builder, debug=False):
         raise BadIngredient(f"Unknown ingredient kind {kind}")
 
     args = []
-
-    print("----converting ingredient -----")
-    print(ingr_dict)
 
     try:
         if kind in ("metric", "dimension"):
@@ -354,9 +135,7 @@ def create_ingredient_from_parsed(ingr_dict, builder, debug=False):
 
         error = {
             "type": "Can not parse field",
-            "extra": {
-                "details": error_msg,
-            },
+            "extra": {"details": error_msg,},
         }
         return InvalidIngredient(error=error)
 
@@ -366,8 +145,6 @@ def create_ingredient_from_parsed(ingr_dict, builder, debug=False):
         # Some internal error while running the Ingredient constructor
         error = {
             "type": "bad_ingredient",
-            "extra": {
-                "details": str(e),
-            },
+            "extra": {"details": str(e),},
         }
         return InvalidIngredient(error=error)
