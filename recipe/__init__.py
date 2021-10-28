09568c2ca9aed31f436f3c7da99250f8018e43ff
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2021-10-28 08:48:32 -0500

    Bump version: 0.28.0 → 0.28.1

--- recipe/__init__.py
+++ recipe/__init__.py
@@ -49,7 +49,7 @@ class NullHandler(logging.Handler):
 
 logging.getLogger(__name__).addHandler(NullHandler())
 
-__version__ = "0.28.0"
+__version__ = "0.28.1"
 
 __all__ = [
     "BadIngredient",
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ install = [
 
 setup(
     name="recipe",
-    version="0.28.0",
+    version="0.28.1",
     description="A construction kit for SQL",
     long_description=(open("README.rst").read()),
     author="Chris Gemignani",
