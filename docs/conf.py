b634d8aa4aaad9d6b65d0ed537158937353f3f51
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2021-11-17 07:56:40 -0600

    Bump version: 0.28.1 → 0.29.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = "Recipe"
 year = "2019"
 author = "Chris Gemignani"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.28.1"
+version = release = "0.29.0"
 
 pygments_style = "trac"
 templates_path = ["."]
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ install = [
 
 setup(
     name="recipe",
-    version="0.28.1",
+    version="0.29.0",
     description="A construction kit for SQL",
     long_description=(open("README.rst").read()),
     author="Chris Gemignani",
