fdd37fa43252b7a372ff90684bbfa992156fbe1e
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2022-12-07 08:14:20 -0500

    Dynamic ingredient calculator

--- recipe/exceptions.py
+++ recipe/exceptions.py
@@ -11,5 +11,12 @@ class InvalidColumnError(Exception):
         self.column_name = kwargs.pop("column_name", None)
         if not args:
             # default exception message
-            args = ['Invalid column "{}"'.format(self.column_name)]
+            args = [f'Invalid column "{self.column_name}"']
         super(InvalidColumnError, self).__init__(*args, **kwargs)
+
+
+class IngredientNotFoundError(KeyError):
+    """Dynamic ingredient can't be found. """
+    def __init__(self, message, ingr):
+        super().__init__(message)
+        self.ingr = ingr
--- recipe/utils/utils.py
+++ recipe/utils/utils.py
@@ -1,6 +1,7 @@
 import math
 import re
 import unicodedata
+from recipe.exceptions import IngredientNotFoundError
 
 from sqlalchemy.sql.functions import FunctionElement
 
@@ -58,3 +59,40 @@ def pad_values(values, prefix="RECIPE-DUMMY-VAL-", bin_size=11):
             return values + added_values
     else:
         return values
+
+
+def calculate_dynamic_ingredient(selections:dict, ingr:str) -> str:
+    """Convert an ingredient string from a dynamic value to a literal
+
+    If selections is {"selection": "student"}
+
+    @selection
+                    Return `student`
+    @selection2|default:"course"
+                    Return `course` (because selection2 was not found)
+    @selection|prefix:id_|default:"course"
+                    Return `id_student`
+    """
+    if not ingr.startswith("@"):
+        return ingr
+    ingredient_parts = ingr[1:].split("|")
+    ingr_name = ingredient_parts[0]
+    ingr_name = selections.get(ingr_name)
+    if isinstance(ingr_name, (list, tuple)):
+        ingr_name = ingr_name[0]
+    directives = {
+        "default": lambda ingr, default: ingr or default,
+        "prefix": lambda ingr, prefix: ingr if ingr is None else prefix + ingr,
+        "suffix": lambda ingr, suffix: ingr if ingr is None else ingr + suffix,
+    }
+    for directive in ingredient_parts[1:]:
+        directive_parts = directive.split(":")
+        directive_name = directive_parts[0]
+        if directive_name not in directives:
+            raise ValueError(f"Unknown ingredient directive: {directive_parts}")
+        ingr_name = directives[directive_name](ingr_name, *directive_parts[1:])
+
+    if not ingr_name:
+        raise IngredientNotFoundError(f"Couldn't find an ingredient based on: {ingr}\nAvailable selections: {selections.keys()}", ingr)
+
+    return ingr_name
