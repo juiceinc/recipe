29a0760a7f27cd3610a883c93d5798aaae40168f
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2023-03-08 08:21:48 -0500

    Fix typo

--- recipe/core.py
+++ recipe/core.py
@@ -144,7 +144,7 @@ class Recipe(object):
 
             # If recipe_caching is installed, apply caching to this query.
             try:
-                from recd.cipe_caching.mappers import FromCache
+                from recipe_caching.mappers import FromCache
 
                 count_query = count_query.options(
                     FromCache(self._cache_region, cache_prefix=self._cache_prefix)
