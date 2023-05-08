213cc2564dbdf627649e6c95f26004a79eb8c13e
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2023-05-08 13:43:14 +0000

    Build test database on start

---
+++ tests/setup_test_data.py
@@ -0,0 +1,199 @@
+import csv
+import io
+import os
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
+from sqlalchemy import (
+    Boolean,
+    Column,
+    Date,
+    DateTime,
+    Float,
+    Integer,
+    MetaData,
+    String,
+    Table,
+    create_engine,
+)
+from yaml import safe_load
+
+
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+sqlite_db = os.path.join(ROOT_DIR, "testdata.db")
+
+
+def strip_columns_from_csv(content: str, ignore_columns: list = None) -> str:
+    if ignore_columns:
+        content = str_dedent(content)
+        rows = list(csv.DictReader(content.splitlines()))
+        for row in rows:
+            for col in ignore_columns:
+                row.pop(col, None)
+        if rows:
+            csv_content = io.StringIO()
+            first_row = rows[0]
+            writer = csv.DictWriter(csv_content, fieldnames=list(first_row.keys()))
+            writer.writeheader()
+            writer.writerows(rows)
+            return csv_content.getvalue().replace("\r\n", "\n").strip("\n")
+
+    return content
+
+
+def str_dedent(s: str) -> str:
+    return "\n".join([x.lstrip() for x in s.split("\n")]).lstrip("\n").rstrip("\n")
+
+
+class SetupData:
+    """Setup databases for testing"""
+
+    def __init__(self, connection_string, **kwargs):
+        self.engine = create_engine(connection_string, **kwargs)
+        self.meta = MetaData(bind=self.engine)
+
+    def load_data(self, table_name, table):
+        """Load data from the data/ directory"""
+        data = safe_load(open(os.path.join(ROOT_DIR, "data", f"{table_name}.yml")))
+        self.engine.execute(table.insert(), data)
+
+    def setup(self):
+        """Set up tables using a connection_string to define an oven.
+
+        Tables are loaded using data in data/{tablename}.yml
+        """
+        self.root_dir = os.path.abspath(os.path.dirname(__file__))
+
+        weird_table_with_column_named_true_table = Table(
+            "weird_table_with_column_named_true", self.meta, Column("true", String)
+        )
+
+        basic_table = Table(
+            "foo",
+            self.meta,
+            Column("first", String),
+            Column("last", String),
+            Column("age", Integer),
+            Column("birth_date", Date),
+            Column("dt", DateTime),
+        )
+
+        datetester_table = Table(
+            "datetester", self.meta, Column("dt", Date), Column("count", Integer)
+        )
+
+        scores_table = Table(
+            "scores",
+            self.meta,
+            Column("username", String),
+            Column("department", String),
+            Column("testid", String),
+            Column("score", Float),
+            Column("test_date", Date),
+        )
+
+        datatypes_table = Table(
+            "datatypes",
+            self.meta,
+            Column("username", String),
+            Column("department", String),
+            Column("testid", String),
+            Column("score", Float),
+            Column("test_date", Date),
+            Column("test_datetime", DateTime),
+            Column("valid_score", Boolean),
+        )
+
+        scores_with_nulls_table = Table(
+            "scores_with_nulls",
+            self.meta,
+            Column("username", String),
+            Column("department", String),
+            Column("testid", String),
+            Column("score", Float),
+            Column("test_date", Date),
+        )
+
+        tagscores_table = Table(
+            "tagscores",
+            self.meta,
+            Column("username", String),
+            Column("tag", String),
+            Column("department", String),
+            Column("testid", String),
+            Column("score", Float),
+        )
+
+        id_tests_table = Table(
+            "id_tests",
+            self.meta,
+            Column("student", String),
+            Column("student_id", Integer),
+            Column("age", Integer),
+            Column("age_id", Integer),
+            Column("score", Float),
+        )
+
+        census_table = Table(
+            "census",
+            self.meta,
+            Column("state", String),
+            Column("sex", String),
+            Column("age", Integer),
+            Column("pop2000", Integer),
+            Column("pop2008", Integer),
+        )
+
+        state_fact_table = Table(
+            "state_fact",
+            self.meta,
+            Column("id", String),
+            Column("name", String),
+            Column("abbreviation", String),
+            Column("sort", String),
+            Column("status", String),
+            Column("occupied", String),
+            Column("notes", String),
+            Column("fips_state", String),
+            Column("assoc_press", String),
+            Column("standard_federal_region", String),
+            Column("census_region", String),
+            Column("census_region_name", String),
+            Column("census_division", String),
+            Column("census_division_name", String),
+            Column("circuit_court", String),
+        )
+
+        self.meta.drop_all()
+        self.meta.create_all()
+        # self.engine.drop_all()
+        # self.engine.create_all()
+        self.load_data(
+            "weird_table_with_column_named_true_table",
+            weird_table_with_column_named_true_table,
+        )
+        self.load_data("basic_table", basic_table)
+        self.load_data("scores_table", scores_table)
+        self.load_data("datatypes_table", datatypes_table)
+        self.load_data("scores_with_nulls_table", scores_with_nulls_table)
+        self.load_data("tagscores_table", tagscores_table)
+        self.load_data("id_tests_table", id_tests_table)
+        self.load_data("census_table", census_table)
+        self.load_data("state_fact_table", state_fact_table)
+
+        # Load the datetester_table with dynamic date data
+        start_dt = date(date.today().year, date.today().month, 1)
+        data = [
+            {"dt": start_dt + relativedelta(months=offset_month), "count": 1}
+            for offset_month in range(-50, 50)
+        ]
+        self.engine.execute(datetester_table.insert(), data)
+
+
+if __name__ == "__main__":
+    d = SetupData(f"sqlite:///{sqlite_db}", echo=True)
+    d.setup()
+
+    d = SetupData(f"postgresql+psycopg2://postgres:postgres@db:5432/postgres", echo=True)
+    d.setup()
