42f1e89e73f5ae4e7da941bed72285da93f5ba12
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2023-08-24 09:30:02 -0400

    Bump version: 0.36.5 → 0.36.6

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = "Recipe"
 year = "2019"
 author = "Chris Gemignani"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.36.5"
+version = release = "0.36.6"
 
 pygments_style = "trac"
 templates_path = ["."]
--- recipe/__init__.py
+++ recipe/__init__.py
@@ -51,7 +51,7 @@ class NullHandler(logging.Handler):
 
 logging.getLogger(__name__).addHandler(NullHandler())
 
-__version__ = "0.36.5"
+__version__ = "0.36.6"
 
 __all__ = [
     "BadIngredient",
